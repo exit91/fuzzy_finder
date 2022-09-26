7d079001b3c174f98b2ff7a1f589b970ccac10f4
Long Huynh Huu <long@putputlaw.de>
2022-09-27 00:23:38 +0200

    refactor tests

--- src/view/scrolling.rs
+++ src/view/scrolling.rs
@@ -60,3 +60,120 @@ impl View for ScrollingView {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const ITEMS: &[&str] = &[
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    ];
+    const FEW_ITEMS: &[&str] = &["A", "B", "C"];
+
+    #[test]
+    fn test_update() {
+        // GIVEN
+        let mut view = ScrollingView::new(8);
+
+        // WHEN
+        let result = view.render(&ITEMS);
+
+        // THEN
+        assert_eq!(result.len(), 8);
+        assert_eq!(result.num_above(), 7); // 0-indexed
+        assert_eq!(result.selected(), Some(&&"A"))
+    }
+
+    #[test]
+    fn test_up() {
+        // GIVEN
+        let mut view = ScrollingView::new(8);
+
+        // WHEN
+        view.up(); // 6
+        view.up(); // 5
+        view.up(); // 4
+        let result3 = view.render(&ITEMS);
+
+        // THEN
+        assert_eq!(result3.len(), 8);
+        assert_eq!(result3.num_above(), 4);
+    }
+
+    #[test]
+    fn test_up_to_extremes() {
+        // GIVEN
+        let mut view = ScrollingView::new(8);
+
+        // WHEN
+        // More than lines_to_show
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        view.up();
+        let result = view.render(&ITEMS);
+
+        // THEN
+        assert_eq!(view.index, 7);
+        assert_eq!(view.skip, 5);
+        assert_eq!(result.len(), 8);
+        assert_eq!(result.num_above(), 0);
+    }
+
+    #[test]
+    fn test_down_at_bottom() {
+        // GIVEN
+        let mut view = ScrollingView::new(8);
+
+        // WHEN
+        view.down(); // 7
+        let result = view.render(&ITEMS);
+
+        // THEN
+        assert_eq!(result.len(), 8);
+        assert_eq!(result.num_above(), 7);
+    }
+
+    #[test]
+    fn test_down() {
+        // GIVEN
+        let mut view = ScrollingView::new(8);
+
+        // WHEN
+        view.up(); // 6
+        view.up(); // 5
+        view.up(); // 4
+        view.down(); // 5
+        let result = view.render(&ITEMS);
+
+        // THEN
+        assert_eq!(result.len(), 8);
+        assert_eq!(result.num_above(), 5);
+    }
+
+    #[test]
+    fn test_few() {
+        // GIVEN
+        let mut view = ScrollingView::new(8);
+
+        // WHEN
+        view.up(); // 6
+        view.up(); // 5
+        view.up(); // 5
+        view.up(); // 5
+        let result = view.render(&FEW_ITEMS);
+
+        // THEN
+        assert_eq!(result.len(), 3);
+        assert_eq!(result.num_above(), 0);
+    }
+}
